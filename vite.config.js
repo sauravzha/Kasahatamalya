c2b5fd82a401a8af20bf224c72230ec87d29aba7
Saurav Jha <Jhasaurav562@gmail.com>
2026-07-13 19:55:14 +0530

    Describe your changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        story: resolve(__dirname, 'story.html'),
+        programs: resolve(__dirname, 'programs.html'),
+        press: resolve(__dirname, 'press.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        donate: resolve(__dirname, 'donate.html'),
+      },
+    },
   },
   server: {
     port: 5173,
